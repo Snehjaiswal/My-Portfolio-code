9d3b7cebf0fe4b8ed71ac2a51b958d25f20e5dd4
Snehjaiswal <snehj121@gmail.com>
2022-04-24 12:58:41 +0530

    today

--- js/main.js
+++ js/main.js
@@ -115,3 +115,21 @@
 	});
 
 })(jQuery);
+
+
+let options = {
+	startAngle: -1.55,
+	size: 150,
+	value: 0.85,
+	fill: {gradient: ['#a445b2', '#fa4299']}
+  }
+  $(".circle .bar").circleProgress(options).on('circle-animation-progress',
+  function(event, progress, stepValue){
+	$(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
+  });
+  $(".js .bar").circleProgress({
+	value: 0.70
+  });
+  $(".react .bar").circleProgress({
+	value: 0.60
+  });
